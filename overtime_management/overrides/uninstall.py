59b36f2129fecd0312e46de8978270bd193a57d2
Manohar Kariyappa <manohar.kariyappa@gmail.com>
2026-06-04 09:54:02 +0000

    Adding the Uninstall py

---
+++ overtime_management/overrides/uninstall.py
@@ -0,0 +1,23 @@
+import frappe
+
+def before_uninstall():
+
+    custom_fields = [
+        "Shift Type-custom_working_hours",
+        "Attendance-custom_overtime_hours",
+        "Salary Slip-custom_total_overtime_hours",
+        "Salary Slip-custom_overtime_amount"
+    ]
+
+    for field in custom_fields:
+        if frappe.db.exists("Custom Field", field):
+            frappe.delete_doc("Custom Field", field, force=True)
+
+    if frappe.db.exists("Salary Component", "Overtime"):
+        frappe.delete_doc(
+            "Salary Component",
+            "Overtime",
+            force=True
+        )
+
+    frappe.db.commit()
