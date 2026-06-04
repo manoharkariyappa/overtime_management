97d2eb5806539737d4ea5b7e52387c8495aa289b
Manohar Kariyappa <manohar.kariyappa@gmail.com>
2026-06-04 07:50:39 +0000

    change the logic in attendance.py

--- overtime_management/overrides/attendance.py
+++ overtime_management/overrides/attendance.py
@@ -3,6 +3,7 @@ import frappe
 def calculate_overtime(doc, method=None):
 
     if not doc.shift or not doc.working_hours:
+        doc.custom_overtime_hours = 0
         return
 
     shift_hours = frappe.db.get_value(
@@ -17,16 +18,19 @@ def calculate_overtime(doc, method=None):
         "holiday_list"
     )
 
-    is_holiday = frappe.db.exists(
-        "Holiday",
-        {
-            "parent": holiday_list,
-            "holiday_date": doc.attendance_date
-        }
-    )
+    is_holiday = False
+
+    if holiday_list:
+        is_holiday = frappe.db.exists(
+            "Holiday",
+            {
+                "parent": holiday_list,
+                "holiday_date": doc.attendance_date
+            }
+        )
 
     if is_holiday:
-        doc.custom_overtime_hours = doc.working_hours
+        doc.custom_overtime_hours = float(doc.working_hours)
     else:
         doc.custom_overtime_hours = max(
             0,
